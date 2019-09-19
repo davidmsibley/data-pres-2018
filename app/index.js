fbf77c896f21813236c445dd653d5e1602f20ad0
David M Sibley <sibley@wisc.edu>
2019-09-19 14:09:58 -0500

    Updates

--- app/index.js
+++ app/index.js
@@ -581,12 +581,12 @@
       /* ~~~~~~~~ Map ~~~~~~~~ */
       //create a map, center it, and set the zoom level. 
       //set zoomcontrol to false because we will add it in a different corner. 
-      const map = this.map = L.map('map', {zoomControl:false}).setView([44.25, -89.9], 7);
+      this.map = L.map('map', {zoomControl:false}).setView([44.25, -89.9], 7);
       this.el = document.querySelector('#map');
        
        /* ~~~~~~~~ Zoom Control ~~~~~~~~ */
       //place a zoom control in the top right: 
-      new L.Control.Zoom({position: 'topright'}).addTo(map);
+      new L.Control.Zoom({position: 'topright'}).addTo(this.map);
 
        
       /* ~~~~~~~~ Basemap Layers ~~~~~~~~ */
@@ -614,15 +614,15 @@
       
       // add the basemap control to the map  
       var basemaps = [voyager, esrisat]; 
-      basemaps[0].addTo(map);
-      map.addControl(L.control.basemaps({
+      basemaps[0].addTo(this.map);
+      this.map.addControl(L.control.basemaps({
          basemaps: basemaps, 
          tileX: 0, 
          tileY: 0, 
          tileZ: 1
       })); 
 
-      let sources = filterLookup.reduce((result, curr) => {
+      this.sources = filterLookup.reduce((result, curr) => {
         if (curr.source && curr.source.geojson) {
           result.push(
             window.fetch(curr.source.geojson)
@@ -634,30 +634,33 @@
         }
         return result;
       }, []);
+    }
 
-      Promise.all(sources).then((responses) => {
-          this.layers = responses.map((res) => {
-            return L.geoJSON(res.data, {
-              name: res.name,
-              pointToLayer: function(geoJsonPoint, latlon) {
-                return new RestylingCircleMarker(latlon, {
-                  weight: 2,
-                  color: res.color,
-                  radius: RestylingCircleMarker.calcRadius(map.getZoom()),
-                  stroke: false,
-                  fill: false
-                });
-              }
-            });
-          });
-          this.layers.forEach(l => l.on('click', (e) => {
-            if (this._highlight !== e.propagatedFrom) {
-              this.fire('interaction', e.propagatedFrom.feature.properties);
-            } else {
-              this.fire('interaction');
+    init() {
+      const map = this.map;
+      return Promise.all(this.sources).then((responses) => {
+        this.layers = responses.map((res) => {
+          return L.geoJSON(res.data, {
+            name: res.name,
+            pointToLayer: function(geoJsonPoint, latlon) {
+              return new RestylingCircleMarker(latlon, {
+                weight: 2,
+                color: res.color,
+                radius: RestylingCircleMarker.calcRadius(map.getZoom()),
+                stroke: false,
+                fill: false
+              });
             }
-          }));
-          this.layers.forEach(l => l.addTo(map));
+          });
+        });
+        this.layers.forEach(l => l.on('click', (e) => {
+          if (this._highlight !== e.propagatedFrom) {
+            this.fire('interaction', e.propagatedFrom.feature.properties);
+          } else {
+            this.fire('interaction');
+          }
+        }));
+        this.layers.forEach(l => l.addTo(this.map));
         let lookup = {};
         this.layers.forEach(function(layer, idx, arr) {
           layer.eachLayer(function(obj) {
@@ -683,7 +686,6 @@
           });
         });
         this._lookup = lookup;
-        this.fire('init');
       });
     }
 
@@ -1695,7 +1697,7 @@
   });
   window.filter = document.querySelector('#filter');
 
-  window.siteMap.once('init', function() {
+  window.siteMap.init().then(function() {
     window.siteData = new SiteData(window.siteMap.layers);
     window.aggrData = siteData.aggrData;
     filter.init(window.siteData.uniques, window.siteMap.layers);
